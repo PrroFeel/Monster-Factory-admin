b624ac5eab784a62515ccd6cc36545c7b5997087
carrtt1 <cej303030@gmail.com>
2022-11-21 16:16:24 +0900

    #10 아이템 목록 조회 페이지 디자인

---
+++ monsterfactory/src/pages/manage/Manage.js
@@ -0,0 +1,8 @@
+function Manage() {
+
+    return (
+        <h1>hello manage page</h1>
+    );
+}
+
+export default Manage;
---
+++ monsterfactory/src/pages/review/Review.js
@@ -0,0 +1,8 @@
+function Review() {
+
+    return (
+        <h1>hello review page</h1>
+    );
+}
+
+export default Review;
