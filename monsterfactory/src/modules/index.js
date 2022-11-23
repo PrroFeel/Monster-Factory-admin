be36bb74d4353f4131dcdd7ec3bf22406ee6ab3e
yunseo <cysdl5604@inu.ac.kr>
2022-11-23 09:50:17 +0900

    #15 심사 목록 컴포넌트 구현

--- monsterfactory/src/modules/index.js
+++ monsterfactory/src/modules/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import memberReducer from "./MemberModule";
 import itemReducer from "./ItemModule";
+import gameReducer from "./GameModule";
 
 const rootReducer = combineReducers({
     memberReducer,
     itemReducer,
+    gameReducer,
 });
 
 export default rootReducer;
--- monsterfactory/src/pages/review/Review.js
+++
@@ -1,8 +0,0 @@
-function Review() {
-
-    return (
-        <h1>hello review page</h1>
-    );
-}
-
-export default Review;
